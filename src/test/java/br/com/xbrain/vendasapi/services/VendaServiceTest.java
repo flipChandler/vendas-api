8c751186c47e296a89e81543efa011374586a7d4
flipChandler <felipe.santos11@hotmail.com>
2021-12-22 19:41:53 -0300

    testes de VendaService

---
+++ src/test/java/br/com/xbrain/vendasapi/services/VendaServiceTest.java
@@ -0,0 +1,20 @@
+package br.com.xbrain.vendasapi.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class VendaServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+}
