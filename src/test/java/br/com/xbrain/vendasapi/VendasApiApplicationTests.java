2ffb6627f3a2363102d6af388b0e87ebede32d36
flipChandler <felipe.santos11@hotmail.com>
2021-12-22 17:48:02 -0300

    test de application.main()

--- src/test/java/br/com/xbrain/vendasapi/VendasApiApplicationTests.java
+++ src/test/java/br/com/xbrain/vendasapi/VendasApiApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class VendasApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		VendasApiApplication.main(new String[] {});
 	}
 
 }
